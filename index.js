5875d197f69d79ba9bf13e9ac3e100ed9059ec7a
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-28 22:55:03 -0600

    Hero Banner Created a HeroBanner and ClearScreen function. This allows for a initial banner screen to be displayed for 5 seconds and automatically go to the main app.

---
+++ index.js
@@ -0,0 +1,10 @@
+import HeroBanner from "./src/HeroBanner.js";
+import ClearScreen from "./src/ClearScreen.js";
+
+function main() {
+    ClearScreen();
+    HeroBanner();
+    ClearScreen();
+    console.log('Begin here\n');
+}
+main();
---
+++ src/HeroBanner.js
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+import figlet from "figlet";
+
+//This function generates a banner
+export default function HeroBanner() {
+  //Show Hero Banner Using the Contessa Figlet font
+  let figText = figlet.textSync("Elite Enterprise\nContent Management System", {
+    font: "contessa",
+    horizontalLayout: "full",
+  });
+  //contessa
+  //Figlet only returns a string.  It has to be shown in the screen via console.log
+  console.log(chalk.bold.red(figText));
+  //Delay for 5 seconds
+  const initTime = Date.now();
+  //Get the current time and subtract the initTime once its over 5000ms (then end delay)
+  while (Date.now() - initTime < 5000) {
+    //Waste clock cycles (5 seconds)
+  }
+}
