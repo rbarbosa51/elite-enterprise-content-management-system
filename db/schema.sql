d791c86cc9ca1fa778d89517aea785c27dc413c8
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-01-29 16:43:19 -0600

    SQL Files In this commit, created the schema.sql and seeds.sql files. This sql files create the database with its 3 tables. As well as populate the database with the pro-populated data from the example video.

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+-- Drop the database if it already exist, then creates eliteEnterpriseCMS
+DROP DATABASE IF EXISTS eliteEnterpriseCMS;
+CREATE DATABASE eliteEnterpriseCMS;
+
+use eliteEnterpriseCMS;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT REFERENCES employee(id)
+);
